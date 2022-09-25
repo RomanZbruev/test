57d598a30746dbf9d7beb796cfda2b32301c62d2
RomanZbruev <100034534+RomanZbruev@users.noreply.github.com>
2022-09-25 15:57:41 +0300

    Delete later/src/main/java/ru/practicum/later/controller directory

--- later/src/main/java/ru/practicum/later/controller/GenericEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.later.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenericEntityRepository extends JpaRepository<GenericEntity,Long> {
-}
